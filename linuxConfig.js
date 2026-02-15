78e2d1daee7d97ef217a18e64cd48ae7b988a520
libzonda <libzonda@outlook.com>
2026-02-15 22:34:53 +0800

    chore: remove temporary files from git tracking

--- linuxConfig.js
+++
@@ -1,7 +0,0 @@
-
-      ;(function(){var params= {"version":"3.2.25","x64DownloadUrl":{"deb":"https://dldir1v6.qq.com/qqfile/qq/QQNT/Linux/QQ_3.2.25_260205_amd64_01.deb","appimage":"https://dldir1v6.qq.com/qqfile/qq/QQNT/Linux/QQ_3.2.25_260205_x86_64_01.AppImage","rpm":"https://dldir1v6.qq.com/qqfile/qq/QQNT/Linux/QQ_3.2.25_260205_x86_64_01.rpm"},"armDownloadUrl":{"deb":"https://dldir1v6.qq.com/qqfile/qq/QQNT/Linux/QQ_3.2.25_260205_arm64_01.deb","rpm":"https://dldir1v6.qq.com/qqfile/qq/QQNT/Linux/QQ_3.2.25_260205_aarch64_01.rpm","appimage":"https://dldir1v6.qq.com/qqfile/qq/QQNT/Linux/QQ_3.2.25_260205_arm64_01.AppImage"},"updateDate":"2026-02-05","loongarchDownloadUrl":"https://dldir1v6.qq.com/qqfile/qq/QQNT/Linux/QQ_3.2.25_260205_loongarch64_01.deb","mipsDownloadUrl":"https://dldir1v6.qq.com/qqfile/qq/QQNT/Linux/QQ_3.2.25_260205_mips64el_01.deb"};
-      typeof callback_1888_config1 === "function" && callback_1888_config1(params)
-      this.define && define(function (require, exports, module) {
-      return params});
-      })()
-    
